f7e4334b95404ea0808bde28e6094f2ea3812e06
Febrianto <febrianto.bekasi@gmail.com>
2022-12-02 20:51:26 +0700

    create example compress image

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 import imagemin from "imagemin";
-import imageminJpegtran from "imagemin-jpegtran";
+
+// lossless
+// import imageminJpegtran from "imagemin-jpegtran";
+// import imageminOptipng from "imagemin-optipng";
+import imageminGifsicle from "imagemin-gifsicle";
+
+// lossy
+import imageminMozjpeg from "imagemin-mozjpeg";
 import imageminPngquant from "imagemin-pngquant";
+import imageminSvgo from "imagemin-svgo";
+// import imageminWebp from "imagemin-webp";
 
 const compressImage = async () => {
-  const files = await imagemin(["public/images/*.{jpg,png,jpeg}"], {
-    destination: "public/images/min",
-    plugins: [
-      imageminJpegtran(),
-      imageminPngquant({
-        quality: [0.6, 0.8],
-      }),
-    ],
-  });
+  const files = await imagemin(
+    ["images/original/*.{jpg,png,jpeg,svg,webP,gif}"],
+    {
+      destination: "images/compress",
+      plugins: [
+        // lossless
+        // imageminJpegtran(),
+        // imageminOptipng(),
+        imageminGifsicle(),
+
+        // lossy
+        imageminSvgo(),
+        // imageminWebp(),
+        imageminMozjpeg(),
+        imageminPngquant(),
+      ],
+    }
+  );
 };
 
 compressImage();
